64cf71d3e6a3e930aaf451c2456c35cc080d2a28
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 02:28:14 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,45 @@
+def instantiate_new_array
+  @my_new_array = []
+  return @my_new_array
+end
+
+def array_with_two_elements
+  @my_two_array = [1, 2]
+  return @my_two_array
+end 
+
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+
+def length_of_array(array)
+  return array.size
+end
+
+
+  
+  
+
+  
